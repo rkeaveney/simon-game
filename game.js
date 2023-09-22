9039de9d3f609b67c3c8a4f4845fd1c6ff32df11
rkeaveney <ronan.keaveney@gmail.com>
2023-09-22 10:51:24 +0100

    Added wrong answer detection & game restart function

--- game.js
+++ game.js
@@ -1,27 +1,65 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+$(document).on("keydown", function() {
+    if (started === false) {
+        $("h1").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4)
+    var randomColour = buttonColours[randomNumber];
+    gamePattern.push(randomColour);
+    userClickedPattern = [];
+    level++;
+
+    $("#" + randomColour).fadeOut(100).fadeIn(100);
+
+    playSound(randomColour);
+    $("h1").text("Level " + level);
+}
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] !== userClickedPattern[currentLevel]) {
+        $("body").addClass("game-over");
+        setTimeout(function () {$("body").removeClass("game-over");}, 200);
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+    else {
+        setTimeout(function () {nextSequence();}, 1000);
+    }
+}
+
+function startOver() {
+    gamePattern = [];
+    level = 0;
+    started = false;
+}
+
+$(".btn").click(function (event) {
+    if (started) {
+        var userChosenColour = event.target.id;
+        animatePress(userChosenColour);
+        playSound(userChosenColour);
+        userClickedPattern.push(userChosenColour);
+        checkAnswer(userClickedPattern.length);
+    }
+});
 
 function playSound (name) {
     var sound = new Audio("sounds/" + name + ".mp3");
     sound.play();
 }
 
-function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4)
-    var randomColour = buttonColours[randomNumber];
-    gamePattern.push(randomColour);
-
-    $("#" + randomColour).fadeOut(100).fadeIn(100);
-
-    playSound(randomColour);
+function animatePress(currentColour) {
+    $("." + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("." + currentColour).removeClass("pressed");}, 100);
 }
-
-$(".btn").click(function (event) {
-    var userChosenColour = event.target.id;
-    playSound(userChosenColour);
-    userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
-});
-
-nextSequence();
